9b771bd3176e2c08d056f8987498b12eff6b991a
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2022-11-08 19:49:07 +0200

    delete comments

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,3 @@
-// import throttle from 'lodash.throttle';
-
-// const formRef = document.querySelector('.feedback-form');
-// const inputRef = document.querySelector('input');
-// const textRef = document.querySelector('textarea');
-
-// const LOCALSTORAGE_KEY = 'feedback-form-state';
-// const localStorageFormData = JSON.parse(
-//   window.localStorage.getItem(LOCALSTORAGE_KEY)
-// );
-
-// let formData = localStorageFormData ? { ...localStorageFormData } : {};
-
-// if (localStorageFormData) {
-//   const { email = '', message = '' } = localStorageFormData;
-//   inputRef.value = email;
-//   textRef.value = message;
-// }
-
-// function onFormInput(e) {
-//   formData[e.target.name] = e.target.value;
-//   window.localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
-// }
-
-// function onFormSubmit(e) {
-//   e.preventDefault();
-//   e.currentTarget.reset();
-//   console.log(formData);
-//   window.localStorage.removeItem(LOCALSTORAGE_KEY);
-// }
-
-// formRef.addEventListener('input', throttle(onFormInput, 500));
-
-// formRef.addEventListener('submit', onFormSubmit);
-
 import throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('.feedback-form');
