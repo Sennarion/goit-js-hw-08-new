1c56c4756ad5d839708bdf3098c52a4a68a298a8
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2022-11-08 19:48:06 +0200

    final

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,38 @@
+// import throttle from 'lodash.throttle';
+
+// const formRef = document.querySelector('.feedback-form');
+// const inputRef = document.querySelector('input');
+// const textRef = document.querySelector('textarea');
+
+// const LOCALSTORAGE_KEY = 'feedback-form-state';
+// const localStorageFormData = JSON.parse(
+//   window.localStorage.getItem(LOCALSTORAGE_KEY)
+// );
+
+// let formData = localStorageFormData ? { ...localStorageFormData } : {};
+
+// if (localStorageFormData) {
+//   const { email = '', message = '' } = localStorageFormData;
+//   inputRef.value = email;
+//   textRef.value = message;
+// }
+
+// function onFormInput(e) {
+//   formData[e.target.name] = e.target.value;
+//   window.localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+// }
+
+// function onFormSubmit(e) {
+//   e.preventDefault();
+//   e.currentTarget.reset();
+//   console.log(formData);
+//   window.localStorage.removeItem(LOCALSTORAGE_KEY);
+// }
+
+// formRef.addEventListener('input', throttle(onFormInput, 500));
+
+// formRef.addEventListener('submit', onFormSubmit);
+
 import throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('.feedback-form');
@@ -9,10 +44,11 @@ const localStorageFormData = JSON.parse(
   window.localStorage.getItem(LOCALSTORAGE_KEY)
 );
 
-let formData = localStorageFormData ? { ...localStorageFormData } : {};
+let formData = {};
 
 if (localStorageFormData) {
-  const { email = '', message = '' } = localStorageFormData;
+  formData = localStorageFormData;
+  const { email = '', message = '' } = formData;
   inputRef.value = email;
   textRef.value = message;
 }
